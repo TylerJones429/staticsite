4447b4c283813c775729a69acb38c947d900ed17
TylerJones429 <TylerJones4292@gmail.com>
2024-08-13 09:43:15 -0400

    working on faulty tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,18 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold", "www.test.com")
         self.assertEqual(node, node2)
 
+        node = TextNode("This is a text node", "italic", "www.test.com")
+        node2 = TextNode("This is a text node", "bold", "www.test.com")
+        self.assertEqual(node, node2)
+
+        node = TextNode("This is a text node", "bold", "www.test.com")
+        node2 = TextNode("This is not a text node", "bold", "www.test.com")
+        self.assertEqual(node, node2)
+
+        node = TextNode("This is a text node", "bold", "www.test.com")
+        node2 = TextNode("This is a text node", "bold", None)
+        self.assertEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
